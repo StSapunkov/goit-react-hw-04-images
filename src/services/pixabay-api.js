1e8d8296afd9537feb76f61a9ac9041f65e02014
StSapunkov <105738333+StSapunkov@users.noreply.github.com>
2023-07-04 19:57:49 +0300

    4-3-7

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -9,22 +9,18 @@ const options = new URLSearchParams([
   ['image_type', 'photo'],
 ]);
 
-export function fetchByName(name) {
-  return fetch(`${URL}q=${name}&key=${KEY}&page=1&${options}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-      return Promise.reject(new Error('Oops Something Went Wrong'));
-  });
+export async function fetchByName(name) {
+  const response = await fetch(`${URL}q=${name}&key=${KEY}&page=1&${options}`);
+  if (response.ok) {
+    return response.json();
+  }
+  return await Promise.reject(new Error('Oops Something Went Wrong'));
 }
 
-export function pagination(name, pageNumber) {
-  return fetch(`${URL}q=${name}&key=${KEY}&page=${pageNumber}&${options}`).then(
-    response => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(new Error('Oops Something Went Wrong'));
-    }
-  );
+export async function pagination(name, pageNumber) {
+  const response = await fetch(`${URL}q=${name}&key=${KEY}&page=${pageNumber}&${options}`);
+  if (response.ok) {
+    return response.json();
+  }
+  return await Promise.reject(new Error('Oops Something Went Wrong'));
 }
